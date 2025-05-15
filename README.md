f8386e5bf6cc4ac0d6c30e47990201fa47a086a6
AJ Rossini <blindglobe@gmail.com>
2025-05-15 09:14:52 +0200

    license and text cleanup, along with git practice

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+
 # evidence
-System for data science, governance, and evidence science, integration, and governance.  Integratibility and (in)coherency
+
+System for data science, governance, and evidence science,
+integration, and governance.  Integratibility and (in)coherency.
+Implementation science (generalised from the pharma domain)
+
+
+
+
+
+
+
---
+++ src/template.lisp
@@ -0,0 +1,17 @@
+;;; -*- Mode:Lisp; Syntax:ANSI-Common-Lisp; Coding:utf-8 -*-
+
+
+;;; Time-stamp: <2025-05-15 09:01:27 tony>
+;;; Creation:   <2025 tony>
+;;; File:       template.lisp
+;;; Author:     AJ Rossini <blindglobe@gmail.com>
+;;; Copyright:  (c)2025--, AJ Rossini.
+;;; License:    MIT license (see ./License.mit)
+;;; Purpose:    Template header file
+
+;;; What is this talk of 'release'? Klingons do not make software
+;;; 'releases'.  Our software 'escapes', leaving a bloody trail of
+;;; designers and quality assurance people in its wake.
+
+;;; This organization and structure is new to the 21st Century
+;;; version..   Think, "21st Century Schizoid Man".
